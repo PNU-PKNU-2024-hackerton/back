f58a3c4282443110ffdd2ea70bf7026156b6029d
wlsgur11 <octopus121@naver.com>
2024-08-23 14:04:01 +0900

    jwt complete

--- src/main/java/com/example/SecurityJWT/config/SecurityConfig.java
+++ src/main/java/com/example/SecurityJWT/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.SecurityJWT.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,8 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import com.example.SecurityJWT.jwt.JWTFilter;
+import com.example.SecurityJWT.jwt.JWTUtil;
 import com.example.SecurityJWT.jwt.LoginFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -20,9 +28,12 @@ public class SecurityConfig {
 	//AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
 	private final AuthenticationConfiguration authenticationConfiguration;
 
-	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+	private final JWTUtil jwtUtil;
+
+	public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
 
 		this.authenticationConfiguration = authenticationConfiguration;
+		this.jwtUtil = jwtUtil;
 	}
 
 	//AuthenticationManager Bean 등록
@@ -41,6 +52,22 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+		http
+			.cors((cors) -> cors
+				.configurationSource(new CorsConfigurationSource() {
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration configuration = new CorsConfiguration();
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+						configuration.setAllowedMethods(Collections.singletonList("*"));
+						configuration.setAllowCredentials(true);
+						configuration.setAllowedHeaders(Collections.singletonList("*"));
+						configuration.setMaxAge(3600L);
+						configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+						return configuration;
+					}
+				}));
 
 		http
 			.csrf((auth) -> auth.disable());
@@ -54,11 +81,18 @@ public class SecurityConfig {
 		http
 			.authorizeHttpRequests((auth) -> auth
 				.requestMatchers("/login", "/", "/join").permitAll()
+				.requestMatchers("/admin").hasRole("ADMIN")
 				.anyRequest().authenticated());
 
-		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+
 		http
-			.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+			.addFilterAfter(new JWTFilter(jwtUtil), LoginFilter.class);
+
+
+		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+
+		http
+			.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
 		http
 			.sessionManagement((session) -> session
--- src/main/java/com/example/SecurityJWT/controller/MainController.java
+++ src/main/java/com/example/SecurityJWT/controller/MainController.java
@@ -1,5 +1,11 @@
 package com.example.SecurityJWT.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,7 +15,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
 	@GetMapping("/")
-	public String main() {
-		return "Main Controller";
+	public String mainP() {
+		//사용자 이름 받아오기
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+
+		//사용자 role받아오기
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+		GrantedAuthority auth = iter.next();
+		String role = auth.getAuthority();
+
+
+		return "Main Controller" + username + role;
 	}
 }
